03546ead0bb9e764653284fd1f473223c37052b4
jsmo998 <123461587+jsmo998@users.noreply.github.com>
2023-04-14 20:30:56 +0100

    methods for ship, alien and bullet added

--- src/main/java/java_module/__asteroids__/Alien.java
+++ src/main/java/java_module/__asteroids__/Alien.java
@@ -3,23 +3,36 @@ package java_module.__asteroids__;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
+import java.util.Random;
+
 public class Alien extends GameObject{
     private Point2D target;
     public Alien(int x, int y){
         // create new polygon - same shape as ship
-        super(new Polygon(-10,-10, 20, 0, -10, -10), x, y,"alien");
-
+        super(new Polygon(-10,-10, 20, 0, -10, -10), x, y,"alien", ScreenUse.SINGULAR);
+        target = new Point2D(0,0);
     }
     public void update(Ship player){
         // set target of alien to player movement
+        target = player.getLocation();
+        updateAngle();
     }
     public Bullet shootBullet(){
         // periodically create bullets to shoot towards target
+        return new Bullet((int) getCharacter().getTranslateX(), (int) getCharacter().getTranslateY());
     }
     private void updateAngle(){
         // update rotation of enemy as target moves
+        double deltaX = target.getX() - getCharacter().getTranslateX();
+        double deltaY = target.getY() - getCharacter().getTranslateY();
+        double angle = Math.atan2(deltaY,deltaX);
+        getCharacter().setRotate(Math.toDegrees(angle)  - 90);
     }
     public static Alien spawnRandom(){
         // create new Alien at random location ------- implement in constructor instead -------
+        Random rnd = new Random();
+        int x = rnd.nextInt(SceneController.WIDTH);
+        int y = rnd.nextInt(SceneController.HEIGHT);
+        return new Alien(x,y);
     }
 }
--- src/main/java/java_module/__asteroids__/Bullet.java
+++ src/main/java/java_module/__asteroids__/Bullet.java
@@ -5,9 +5,6 @@ import javafx.scene.shape.Polygon;
 public class Bullet extends GameObject{
     public Bullet(int x, int y){
         // constructor creates new polygon in Bullet dimensions
-        super(new Polygon(1, -1, 1, 1, -1, 1, -1, -1), x, y, "bullet");
-    }
-    public void move(){
-        // unlike super.move(), bullets will not move on the infinite screen but finish at edge
+        super(new Polygon(1, -1, 1, 1, -1, 1, -1, -1), x, y, "bullet", ScreenUse.SINGULAR);
     }
 }
