3fa830cce78bdfdcfc1c6b6cc0161cc75ac83d28
jsmo998 <123461587+jsmo998@users.noreply.github.com>
2023-04-14 16:56:02 +0100

    Initial commit

---
+++ src/main/java/java_module/__asteroids__/HelloController.java
@@ -0,0 +1,14 @@
+package java_module.__asteroids__;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
