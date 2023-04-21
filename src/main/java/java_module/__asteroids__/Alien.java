b45c133f2d475bf91a920cc496d6393edc2f58be
jsmo998 <123461587+jsmo998@users.noreply.github.com>
2023-04-21 11:22:16 +0100

    cleaned up alien movement syntax

--- src/main/java/java_module/__asteroids__/Alien.java
+++ src/main/java/java_module/__asteroids__/Alien.java
@@ -7,12 +7,7 @@ import java.util.Random;
 
 public class Alien extends GameObject{
     private Point2D target;
-<<<<<<< HEAD
-    private Point2D movement;
     private static final double SPEED =1 ;
-    double changeX = Math.cos(Math.toRadians(20));
-    double changeY = Math.sin(Math.toRadians(20));
-=======
     public enum AlienSpawn {
         // used to decide which side of the screen the alien will appear
         TOP,
@@ -20,7 +15,6 @@ public class Alien extends GameObject{
         LEFT,
         RIGHT,
     }
->>>>>>> 352c8eb99f37a2dfe80422792332e94e109e43d2
     public Alien(int x, int y){
         // create new polygon - same shape as ship
         super(new Polygon(-15, 0, 0, 10, 15, 0, 0, -10), x, y,"alien", ScreenUse.SINGULAR);
@@ -48,53 +42,20 @@ public class Alien extends GameObject{
         double angle = Math.atan2(deltaY,deltaX);
         getCharacter().setRotate(Math.toDegrees(angle)  - 90);
     }
-//    public static Alien spawnRandom(){
-//        Random rnd = new Random();
-//        int x = 0;
-//        int y = 0;
-//        AlienSpawn side = AlienSpawn.values()[rnd.nextInt(AlienSpawn.values().length)];
-//        switch (side){
-//            case TOP -> x = rnd.nextInt(800);
-//            case BOTTOM -> {x = rnd.nextInt(800); y = 600;}
-//            case LEFT -> y = rnd.nextInt(600);
-//            case RIGHT -> {x=800; y = rnd.nextInt(600);}
-//        }
-//        // set movement to point to other side of screen
-//        return new Alien(x, y);
-//    }
-//    public void move(){
-//        // extend super.move to remove ship when beyond screen bounds
-//        if (this.beyondScreenBounds()){
-//            this.setLife(false);
-//        }
-//        super.move();
-////        super.getCharacter().setRotate(super.getCharacter().getRotate());
-//    }
-public static Alien spawnRandom() {
-    Random rnd = new Random();
-    int edge = 1; // 0 = top, 1 = right, 2 = bottom, 3 = left
-    int x, y;
-    switch (edge) {
-        case 0: // top edge
-            x = rnd.nextInt(SceneController.WIDTH);
-            y = 10;
-            break;
-        case 1: // right edge
-            x = SceneController.WIDTH;
-            y = rnd.nextInt(SceneController.HEIGHT);
-            break;
-        case 2: // bottom edge
-            x = rnd.nextInt(SceneController.WIDTH);
-            y = SceneController.HEIGHT;
-            break;
-        default: // left edge
-            x = 0;
-            y = rnd.nextInt(SceneController.HEIGHT);
-            break;
+    public static Alien spawnRandom(){
+        Random rnd = new Random();
+        int x = 0;
+        int y = 0;
+        AlienSpawn side = AlienSpawn.values()[rnd.nextInt(AlienSpawn.values().length)];
+        switch (side){
+            case TOP -> x = rnd.nextInt(800);
+            case BOTTOM -> {x = rnd.nextInt(800); y = 600;}
+            case LEFT -> y = rnd.nextInt(600);
+            case RIGHT -> {x=800; y = rnd.nextInt(600);}
+        }
+        // set movement to point to other side of screen
+        return new Alien(x, y);
     }
-
-    return new Alien(x, y);
-}
 
     public void move() {
         if (this.beyondScreenBounds()) {
@@ -105,21 +66,9 @@ public static Alien spawnRandom() {
             double spawnY = this.getSpawnY();
 
             // Determine direction towards opposite corner
-            if (this.getCharacter().getTranslateX() < spawnX) {
-                dx = 1;
-            } else if (this.getCharacter().getTranslateX() > spawnX) {
-                dx = -1;
-            } else {
-                dx = 0;
-            }
+            dx = Double.compare(spawnX, this.getCharacter().getTranslateX());
 
-            if (this.getCharacter().getTranslateY() < spawnY) {
-                dy = 1;
-            } else if (this.getCharacter().getTranslateY() > spawnY) {
-                dy = -1;
-            } else {
-                dy = 0;
-            }
+            dy = Double.compare(spawnY, this.getCharacter().getTranslateY());
 
             // Move the Alien towards opposite corner
             this.getCharacter().setTranslateX(this.getCharacter().getTranslateX() + dx*SPEED);
